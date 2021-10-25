f522f27ff712045679c1a816f7c2231d19ce1f17
marcio30peres <marcio30peres@gmail.com>
2021-10-25 13:06:41 -0300

    Primeiro commit: aulas até mock de api

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+import { createServer } from 'miragejs'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+createServer({
+    routes() {
+        this.namespace = 'api'
+
+        this.get('/transactions', () => {
+            return [
+                {
+                    id: 1,
+                    title: 'Transaction',
+                    amount: 400,
+                    type: 'deposit',
+                    category: 'food',
+                    createdAt: new Date()
+                }
+            ]
+        })
+    }
+})
+
+ReactDOM.render(<App />, document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
